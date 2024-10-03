a15ead40c4d75c0a24157d0487cf34ca2f45c2e6
Victor Favretto <143458351+vfavretto@users.noreply.github.com>
2024-10-03 20:18:12 -0300

    TecProgII - New Project BankAcc

---
+++ TecProgII/ProjetoBanco/DAO/src/DAO/connectDAO.java
@@ -0,0 +1,34 @@
+package DAO;
+
+import java.util.List;
+import java.sql.Statement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+// import javax.swing.table.AbstractTableModel;
+
+public class connectDAO {
+        Connection con;
+    public Connection connectDB(){
+        JOptionPane.showMessageDialog(null, "Inicia a classe para conexão com SQL SERVER!");
+ 
+        String caminho = "jdbc:sqlserver://localhost:1433;databaseName=MOV_CONTA_CORRENTE;encrypt=true;trustServerCertificate=true;"; 
+        String usuario = "victor";
+        String senha = "Abc@123";
+        try {
+            con = DriverManager.getConnection(caminho, usuario, senha);
+            JOptionPane.showMessageDialog(null, "Conectado com sucesso!");
+        } catch (SQLException erro) {
+            JOptionPane.showMessageDialog(null, "Erro de conexão, connectDAO - Mensagem => "+erro.getMessage());
+            JOptionPane.showMessageDialog(null, "\n Erro de conexão, connectDAO - Estado => "+erro.getSQLState());
+            JOptionPane.showMessageDialog(null, "\n Erro de conexão, connectDAO - Código => "+erro.getErrorCode());
+        }
+        return con;
+        // con.close();
+    }
+}
